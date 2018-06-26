c02f0b62a4724642e46176d1e1e920bae3b028a9
Halil İbrahim Kalyoncu <ibrahimkalyoncu@hotmail.com.tr>
2018-06-26 17:11:03 +0300

    temp commit

--- src/AspNetCore.Mvc.Fragments/AspNetCore.Mvc.Fragments/Constants.cs
+++ src/AspNetCore.Mvc.Fragments/AspNetCore.Mvc.Fragments/Constants.cs
@@ -6,5 +6,7 @@
         public const string FragmentTypeSuffix = "Fragment";
         public const string HttpContexItemsFragmentCollectionKey = "FragmentCollection";
         public const string HtmlIdAttribute = "id";
+        public const string FragmentStylePlaceHolder = "<!-----FRAGMENT_STYLES----->";
+        public const string FragmentScriptPlaceHolder = "<!-----FRAGMENT_SCRIPTS----->";
     }
 }
---
+++ src/AspNetCore.Mvc.Fragments/AspNetCore.Mvc.Fragments/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AspNetCore.Mvc.Fragments.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static HtmlString FragmentStyles(this IHtmlHelper helper)
+        {
+            return new HtmlString(Constants.FragmentStylePlaceHolder);
+        }
+
+        public static HtmlString FragmentScripts(this IHtmlHelper helper)
+        {
+            return new HtmlString(Constants.FragmentScriptPlaceHolder);
+        }
+    }
+}
